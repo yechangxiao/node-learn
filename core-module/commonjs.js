6f2e9c1ad146c990ab50ee72a3a7f3c428440f18
cxYee <yechangxiaoycx@qq.com>
2022-09-16 23:41:54 +0800

    [feat]: commonjs的模块加载机制

---
+++ core-module/commonjs.js
@@ -0,0 +1,99 @@
+// ```md
+// ### 加载流程
+// - #### 路径分析：确定目标模块位置
+// - #### 文件定位：确定目标模块中的具体文件
+// - #### 编译执行：对模块内容进行编译，返回可用exports对象
+// ```
+
+const fs = require('fs')
+const path = require('path')
+const vm = require('vm')
+
+function Module(id) {
+  this.id = id
+  this.exports = {}
+  console.log('1111') // 缓存优先，多次执行只加载一次
+  
+}
+
+Module._resolveFilename = function (filename) {
+  let absPath = path.resolve(__dirname, filename)
+  // 判断当前路径对应的内容是否存在
+  if (fs.existsSync(absPath)) {
+    return absPath
+  } else {
+    // 文件点位
+    const suffix = Object.keys(Module._extensions)
+    for (const item of suffix) {
+      const newPath = absPath + item
+      if (fs.existsSync(newPath)) {
+        return newPath
+      }
+    }
+  }
+  throw new Error(`${filename} is not exists`)
+}
+
+Module._extensions = {
+  '.js'(module) {
+    // 读取
+    let content = fs.readFileSync(module.id, 'utf-8')
+
+    // 包装
+    content = Module.wrapper[0] + content + Module.wrapper[1]
+
+    // vm
+    let compileFn = vm.runInThisContext(content)
+
+    // 准备参数的值
+    let exports = module.exports
+    let dirname = path.dirname(module.id)
+    let filename = module.id
+
+    // 调用
+    compileFn.call(exports, exports, myRequire, module, filename, dirname)
+  },
+  '.json'(module) {
+    let content = JSON.parse(fs.readFileSync(module.id, 'utf-8'))
+    module.exports = content
+  }
+}
+
+Module.wrapper = [
+  '(function(exports, require, module, __filename, __dirname) {',
+  '})'
+]
+
+Module._cache = {}
+
+Module.prototype.load = function () {
+  let extname = path.extname(this.id)
+  Module._extensions[extname](this)
+}
+
+function myRequire(filename) {
+  // 1 绝对路径
+  let mPath = Module._resolveFilename(filename)
+  
+  // 2 缓存优先
+  let cacheModule = Module._cache[mPath]
+  if (cacheModule) {
+    return cacheModule.exports
+  }
+
+  // 3 创建空对象加载目标模块
+  let module = new Module(mPath)
+
+  // 4 缓存已加载过的模块
+  Module._cache[mPath] = module
+
+  // 5 执行加载(编译执行)
+  module.load()
+
+  // 6 返回数据
+  return module.exports
+}
+
+let obj = myRequire('./test')
+// let obj1 = myRequire('./test') // 测试缓存优先
+console.log(obj)
