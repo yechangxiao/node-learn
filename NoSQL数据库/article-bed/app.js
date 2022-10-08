6244c227acd44eb95a170ff97e757bfcddc2f4ec
cxYee <yechangxiaoycx@qq.com>
2022-10-08 23:59:16 +0800

    [feat]: 创建文章

--- NoSQL数据库/article-bed/app.js
+++ NoSQL数据库/article-bed/app.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
+
+const connectUri = 'mongodb://localhost:27017'
+const dbClient = new MongoClient(connectUri)
 
 const app = express()
 // 配置解析请求体数据 application/json
@@ -9,15 +13,23 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.post('/articles', (req, res) => {
-  console.log(req.body)
+app.post('/articles', async (req, res) => {
+  // console.log(req.body)
   const { article } = req.body
   if (!article || !article.title || !article.description || !article.body) {
     return res.status(422).json({
       error: '请求参数不符合规则要求'
     })
   }
-  res.send('post/articles')
+  await dbClient.connect()
+  const collection = dbClient.db('test').collection('articles')
+  article.createdAt = new Date()
+  article.updatedAt = new Date()
+  const ret = await collection.insertOne(article)
+  article._id = ret.insertedId
+  res.status(201).json({
+    article
+  })
 })
 
 app.get('/articles', (req, res) => {
