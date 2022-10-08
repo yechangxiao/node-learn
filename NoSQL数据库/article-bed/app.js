32fa93cf1fc2b11f1a13bddbc00482138749882f
cxYee <yechangxiaoycx@qq.com>
2022-10-08 23:48:23 +0800

    [feat]: 创建路由和解析json格式参数

--- NoSQL数据库/article-bed/app.js
+++ NoSQL数据库/article-bed/app.js
@@ -1,9 +1,39 @@
 const express = require('express')
 
 const app = express()
+// 配置解析请求体数据 application/json
+// 会把请求解析到的数据放到req.body中
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.post('/articles', (req, res) => {
+  console.log(req.body)
+  const { article } = req.body
+  if (!article || !article.title || !article.description || !article.body) {
+    return res.status(422).json({
+      error: '请求参数不符合规则要求'
+    })
+  }
+  res.send('post/articles')
+})
+
+app.get('/articles', (req, res) => {
+  res.send('get/articles')
+})
+
+app.get('/articles/:id', (req, res) => {
+  res.send('get/articles/id')
+})
+
+app.patch('/articles/:id', (req, res) => {
+  res.send('patch/articles/id')
+})
+
+app.delete('/articles/:id', (req, res) => {
+  res.send('delete/articles/id')
 })
 
 app.listen(3000, () => {
