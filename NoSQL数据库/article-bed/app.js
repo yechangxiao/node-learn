e02f8ea031843c3b0003cbd338fe7ce0cb51bb91
cxYee <yechangxiaoycx@qq.com>
2022-10-09 16:22:38 +0800

    [feat]: 统一错误处理和获取文章列表

--- NoSQL数据库/article-bed/app.js
+++ NoSQL数据库/article-bed/app.js
@@ -13,26 +13,43 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.post('/articles', async (req, res) => {
-  // console.log(req.body)
-  const { article } = req.body
-  if (!article || !article.title || !article.description || !article.body) {
-    return res.status(422).json({
-      error: '请求参数不符合规则要求'
+app.post('/articles', async (req, res, next) => {
+  try {
+    // console.log(req.body)
+    const { article } = req.body
+    if (!article || !article.title || !article.description || !article.body) {
+      return res.status(422).json({
+        error: '请求参数不符合规则要求'
+      })
+    }
+    await dbClient.connect()
+    const collection = dbClient.db('test').collection('articles')
+    article.createdAt = new Date()
+    article.updatedAt = new Date()
+    const ret = await collection.insertOne(article)
+    article._id = ret.insertedId
+    res.status(201).json({
+      article
     })
+  } catch (error) {
+    next(error)
   }
-  await dbClient.connect()
-  const collection = dbClient.db('test').collection('articles')
-  article.createdAt = new Date()
-  article.updatedAt = new Date()
-  const ret = await collection.insertOne(article)
-  article._id = ret.insertedId
-  res.status(201).json({
-    article
-  })
 })
 
-app.get('/articles', (req, res) => {
+app.get('/articles', async (req, res) => {
+  try {
+    await dbClient.connect()
+    const collection = dbClient.db('test').collection('articles')
+    const ret = await collection.find()
+    const articles = await ret.toArray()
+    const articlesCount = await collection.countDocuments()
+    res.status(200).json({
+      articles,
+      articlesCount
+    })
+  } catch (error) {
+    next(error)
+  }
   res.send('get/articles')
 })
 
@@ -46,6 +63,14 @@ app.patch('/articles/:id', (req, res) => {
 
 app.delete('/articles/:id', (req, res) => {
   res.send('delete/articles/id')
+})
+
+// 它之前的所有路由中调用next(err)就会进入这里
+// 4个参数缺一不可
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: err.message
+  })
 })
 
 app.listen(3000, () => {
