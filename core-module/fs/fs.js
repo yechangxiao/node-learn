e3c679bfc59fd84e727223d27dbaf51170451e00
cxYee <yechangxiaoycx@qq.com>
2023-01-05 23:26:03 +0800

    [note]: 文件操作api

---
+++ core-module/fs/fs.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const fs = require('fs')
+
+// readFile
+// fs.readFile(path.resolve(__dirname, 'data.txt'), 'utf-8', (err, data) => {
+//   console.log(err)
+//   console.log(data)
+// })
+
+// writeFile
+// 相对路径为当前工作目录，不是当前执行的目录
+// fs.writeFile('data.txt', '你好 世界', (err) => {
+//   if (!err) {
+//     fs.readFile('data.txt', 'utf-8', (err, data) => {
+//       console.log(data)
+//     })
+//   }
+// })
+
+// fs.writeFile('data.txt', '111', {
+//   mode: 438,
+//   // 通过权限位设置，不清空当前内容，在前面写入
+//   flag: 'r+',
+//   encoding: 'utf-8'
+// }, (err) => {
+//   if (!err) {
+//     fs.readFile('data.txt', 'utf-8', (err, data) => {
+//       console.log(data)
+//     })
+//   }
+// })
+
+// appendFile
+// fs.appendFile('data.txt', '2333', (err) => {
+//   console.log('写入成功')
+// })
+
+// copyFile
+// fs.copyFile('data.txt', 'test.txt', (err) => {
+//   console.log('拷贝成功')
+// })
+
+// watchFile
+fs.watchFile('data.txt', { interval: 20 }, (curr, prev) => {
+  if (curr.mtime !== prev.mtime) {
+    console.log('文件被修改了')
+    fs.unwatchFile('data.txt')
+  }
+})
